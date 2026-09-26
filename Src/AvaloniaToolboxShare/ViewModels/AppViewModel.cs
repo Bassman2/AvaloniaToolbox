8580090050c1516e15d544684fc875b62f6c1b5f
Ralf Beckers <ralf.beckers@elektrobit.com>
2026-09-26 14:49:25 +0200

    Update

---
+++ Src/AvaloniaToolboxShare/ViewModels/AppViewModel.cs
@@ -0,0 +1,28 @@
+﻿namespace AvaloniaToolbox.ViewModels;
+
+public partial class AppViewModel : ObservableObject
+{
+    [ObservableProperty]
+    public partial string StatusText { get; set; } = "Ready";
+
+    [RelayCommand]
+    public void OnExit()
+    {
+        if (Application.Current?.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+        {
+            desktop.Shutdown();
+        }
+    }
+
+    //=> applicationService.ExitApplication();
+
+    //public ThemeMode CurrentTheme => applicationService.ThemeMode;
+
+
+    //[RelayCommand]
+    //public void OnSetMode(ThemeMode themeMode) => applicationService.ThemeMode = themeMode;
+
+
+    //[RelayCommand]
+    //public void OnHelp() => applicationService.OpenUrl(applicationHelpUrl);
+}
---
+++ Src/AvaloniaToolboxShare/Views/AppView.cs
@@ -0,0 +1,67 @@
+﻿using AvaloniaToolbox.Services;
+
+namespace AvaloniaToolbox.Views;
+
+public partial class AppView : Window
+{
+    private readonly ISettingsService settingsService;
+
+    private NativeMenu? docMenu;
+
+    public AppView()
+    {
+        this.settingsService = Ioc.Default.GetRequiredService<ISettingsService>();
+    }
+
+    protected override void OnOpened(System.EventArgs e)
+    {
+        base.OnOpened(e);
+
+        // Geometrie aus den Einstellungen laden
+        var settings = settingsService.Current;
+
+        Width = settings.WindowWidth;
+        Height = settings.WindowHeight;
+        WindowState = settings.LastWindowState;
+
+        if (settings.WindowX != -1 && settings.WindowY != -1)
+        {
+            Position = new PixelPoint(settings.WindowX, settings.WindowY);
+        }
+
+        ////////////////////////////////////////////
+
+        //if (DataContext is MainViewModel vm && vm.Documentations != null)
+        //{
+        //    var docMenuItem = NativeMenu.GetMenu(this)?.Items.OfType<NativeMenuItem>().FirstOrDefault(item => item.Header?.ToString() == "_Documentation");
+        //    if (docMenuItem != null)
+        //    {
+        //        docMenu = docMenuItem.Menu ??= new();
+
+        //        RebuildNativeMenu();
+        //    }
+        //    vm.PropertyChanged += OnViewModelPropertyChanged;
+        //}
+    }
+
+    protected override void OnClosing(WindowClosingEventArgs e)
+    {
+        var settings = settingsService.Current;
+
+        // Zustand und Größe sichern
+        settings.LastWindowState = WindowState;
+
+        if (WindowState == WindowState.Normal)
+        {
+            settings.WindowWidth = Width;
+            settings.WindowHeight = Height;
+            settings.WindowX = Position.X;
+            settings.WindowY = Position.Y;
+        }
+
+        // Einstellungen speichern
+        settingsService.Save();
+
+        base.OnClosing(e);
+    }
+}
